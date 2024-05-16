490bac001f719d6771de1c2ba677b27366c62228
priyakalidaspawar <92677580+priyakalidaspawar@users.noreply.github.com>
2024-05-16 17:05:22 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -31,4 +31,4 @@ def profile():
         return render_template('profile.html', user=user_credentials)
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port="8080")
+    app.run(debug=True,host="0.0.0.0",port="5000")
